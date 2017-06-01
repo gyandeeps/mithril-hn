2f26a3d23223b5e82625d956532ffd01219d1ee9
Gyandeep Singh <gyandeeps@gmail.com>
2017-05-31 22:30:27 -0500

    Fix: make connection https to service provider

--- src/js/data/fetch-data.js
+++ src/js/data/fetch-data.js
@@ -2,20 +2,20 @@ import { request } from "mithril/request";
 
 export const getNew = (id = 1) => request({
     type: "GET",
-    url: `http://node-hnapi.herokuapp.com/news?page=${id}`
+    url: `https://node-hnapi.herokuapp.com/news?page=${id}`
 });
 
 export const getShow = (id = 1) => request({
     type: "GET",
-    url: `http://node-hnapi.herokuapp.com/show?page=${id}`
+    url: `https://node-hnapi.herokuapp.com/show?page=${id}`
 });
 
 export const getAsk = (id = 1) => request({
     type: "GET",
-    url: `http://node-hnapi.herokuapp.com/ask?page=${id}`
+    url: `https://node-hnapi.herokuapp.com/ask?page=${id}`
 });
 
 export const getJobs = (id = 1) => request({
     type: "GET",
-    url: `http://node-hnapi.herokuapp.com/jobs?page=${id}`
+    url: `https://node-hnapi.herokuapp.com/jobs?page=${id}`
 });
